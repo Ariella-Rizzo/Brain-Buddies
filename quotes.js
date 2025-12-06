26da2812bbecf24d69facc7fc5def9b155d60400
Ariella-Rizzo <alrizzo@email.sc.edu>
2025-12-05 23:29:16 -0500

    Update quotes with additional entries and corrections

--- quotes.js
+++ quotes.js
@@ -15,10 +15,15 @@ var randomQ = randomInt(0, 10);
 function getQuote(n) {
    var quotes = [
       "It's okay to not be okay -Unknown",
-      "You are braver than you believe, stronger than you seem -Christopher Robin/Winnie the Pooh",
+      "You are braver than you believe, stronger than you seem, and smarter than you think.-Christopher Robin/Winnie the Pooh",
       "There is hope, even when your brain tells you there isn’t. -John Green",
       "You can’t stop the waves, but you can learn to surf -Jon Kabat-Zinn",
-      "You are your best thing. -Tony Morrison"
+      "You are your best thing. -Tony Morrison",
+      "You don’t have to see the whole staircase, just take the first step -Martin Luther King Jr.",
+      "The sun will rise, and we will try again -Unknown",
+      "The place to find who you are is where you stand -George Mumford",
+      "Remember that wherever your heart is, there you will find your treasure -Paulo Coelho",
+      "I will have to remember ‘I am here today to cross the swamp, not to fight all the alligators -Rosamund and Benjamin Zander"
    ];
    
    return quotes[n];
